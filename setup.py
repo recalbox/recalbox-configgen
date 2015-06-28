240db080abbb28d1014e699604ced3789269bb9f
digitalLumberjack <digitallumberjack@gmail.com>
2015-06-28 08:28:03 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+setup(name='recalbox-configgen',
+      version='1.0',
+      py_modules=['configgen'],
+      packages=['configgen', 'configgen.generators', 'configgen.generators.libretro', 'configgen.settings','configgen.utils'],
+      
+      )
