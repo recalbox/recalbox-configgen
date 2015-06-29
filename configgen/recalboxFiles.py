0013296fd5a2d11acb7a3123d062c5e08f69216f
digitalLumberjack <digitallumberjack@gmail.com>
2015-06-29 19:40:00 +0200

    changed recalboxfiles tests

--- configgen/recalboxFiles.py
+++ configgen/recalboxFiles.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 
-esInputs = "/home/matthieu/.emulationstation/es_input.cfg"
-esSettings = '/home/matthieu/.emulationstation/es_settings.cfg'
-recalboxConf = "/home/matthieu/recalbox/recalbox.conf"
+esInputs = '/root/.emulationstation/es_input.cfg'
+esSettings = '/root/.emulationstation/es_settings.cfg'
+recalboxConf = '/recalbox/share/system/recalbox.conf'
 
-retroarchRoot = "/home/matthieu/recalbox/configs/retroarch"
+retroarchRoot = '/recalbox/configs/retroarch'
 retroarchCustom = retroarchRoot + '/retroarchcustom.cfg'
 retroarchCustomOrigin = retroarchRoot + "/retroarchcustom.cfg.origin"
 retroarchBin = "retroarch"
 retroarchCores = "/usr/lib/libretro/"
-shadersRoot = "/home/matthieu/recalbox/share/shaders/presets/"
+shadersRoot = "/recalbox/share/shaders/presets/"
+
