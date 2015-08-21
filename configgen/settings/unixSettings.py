b24703865d79bf6a2ef4938b7c1c7bc31e617cca
digitalLumberjack <digitallumberjack@gmail.com>
2015-08-21 16:03:21 +0200

    remove all controls config before new one

--- configgen/settings/unixSettings.py
+++ configgen/settings/unixSettings.py
@@ -27,19 +27,18 @@ class UnixSettings():
                             return m.group(1)
             return default
 
-
     def save(self, name, value):
         os.system(
             "sed -i \"s|^{}\?{}{}=.*|{}{}={}{}|g\" {}".format(self.comment, name, self.separator, name,
-                                                             self.separator, self.separator,
-                                                             value, self.settingsFile))
+                                                              self.separator, self.separator,
+                                                              value, self.settingsFile))
         if self.load(name) is None:
             with open(self.settingsFile, "a+") as settings:
                 settings.write("\n{}{}={}{}".format(name, self.separator, self.separator, value))
 
-
     def disable(self, name):
-        os.system("sed -i \"s|^.*\({}{}\?=.*\)|{}\\1|g\" {}".format(name, self.separator, self.comment, self.settingsFile))
+        os.system(
+            "sed -i \"s|^.*\({}{}\?=.*\)|{}\\1|g\" {}".format(name, self.separator, self.comment, self.settingsFile))
 
     def disableAll(self, name):
         os.system("sed -i \"s|^.*\({}.*\)|{}\\1|g\" {}".format(name, self.comment, self.settingsFile))
