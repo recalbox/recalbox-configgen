5e04f07fcfbc08302cf484a4274e8b7575c9ac8f
DigitalLumberjack <digitallumberjack@gmail.com>
2015-11-22 15:35:40 +0100

    corrected recalbox files

--- configgen/recalboxFiles.py
+++ configgen/recalboxFiles.py
@@ -1,29 +1,30 @@
 #!/usr/bin/env python
 
-esInputs = "/home/matthieu/.emulationstation/es_input.cfg"
-esSettings = '/home/matthieu/.emulationstation/es_settings.cfg'
-recalboxConf = "/home/matthieu/recalbox/recalbox.conf"
+esInputs = '/root/.emulationstation/es_input.cfg'
+esSettings = '/root/.emulationstation/es_settings.cfg'
+recalboxConf = '/recalbox/share/system/recalbox.conf'
 
-retroarchRoot = "/home/matthieu/recalbox/configs/retroarch"
+retroarchRoot = '/recalbox/configs/retroarch'
 retroarchCustom = retroarchRoot + '/retroarchcustom.cfg'
 retroarchCustomOrigin = retroarchRoot + "/retroarchcustom.cfg.origin"
 retroarchCoreCustom = retroarchRoot + "/cores/retroarch-core-options.cfg"
 
 retroarchBin = "retroarch"
 retroarchCores = "/usr/lib/libretro/"
-shadersRoot = "/home/matthieu/recalbox/share/shaders/presets/"
+shadersRoot = "/recalbox/share/shaders/presets/"
 shadersExt = '.gplsp'
 libretroExt = '_libretro.so'
 
-fbaRoot = '/home/matthieu/recalbox/configs/fba/'
+fbaRoot = '/recalbox/configs/fba/'
 fbaCustom = fbaRoot + 'fba2x.cfg'
 fbaCustomOrigin = fbaRoot + 'fba2x.cfg.origin'
 fba2xBin = '/usr/bin/fba2x'
 
-mupenCustom = "/home/matthieu/recalbox/configs/mupen64/mupen64plus.cfg"
-shaderPresetRoot = "/home/matthieu/recalbox/share/system/shadersets/"
+mupenCustom = "/recalbox/configs/mupen64/mupen64plus.cfg"
 
-kodiJoystick = '/home/matthieu/.kodi/userdata/keymaps/recalbox.xml'
-kodiMapping  = '/home/matthieu/recalbox/configs/kodi/input.xml'
+shaderPresetRoot = "/recalbox/configs/shadersets/"
 
-kodiBin  = 'echo'
+kodiJoystick = '/root/.kodi/userdata/keymaps/recalbox.xml'
+kodiMapping  = '/recalbox/configs/kodi/input.xml'
+
+kodiBin  = '/usr/lib/kodi/kodi.bin'
