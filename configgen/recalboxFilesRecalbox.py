77259af098e6476850c3a2cfed62a43853680eb9
digitalLumberjack <digitallumberjack@gmail.com>
2015-11-29 17:02:12 +0100

    added config real path

--- configgen/recalboxFilesRecalbox.py
+++ configgen/recalboxFilesRecalbox.py
@@ -4,7 +4,7 @@ esInputs = '/recalbox/share/system/.emulationstation/es_input.cfg'
 esSettings = '/recalbox/share/system/.emulationstation/es_settings.cfg'
 recalboxConf = '/recalbox/share/system/recalbox.conf'
 
-retroarchRoot = '/recalbox/share/configs/retroarch'
+retroarchRoot = '/recalbox/share/system/configs/retroarch'
 retroarchCustom = retroarchRoot + '/retroarchcustom.cfg'
 retroarchCustomOrigin = retroarchRoot + "/retroarchcustom.cfg.origin"
 retroarchCoreCustom = retroarchRoot + "/cores/retroarch-core-options.cfg"
@@ -15,16 +15,16 @@ shadersRoot = "/recalbox/share/shaders/presets/"
 shadersExt = '.gplsp'
 libretroExt = '_libretro.so'
 
-fbaRoot = '/recalbox/share/configs/fba/'
+fbaRoot = '/recalbox/share/system/configs/fba/'
 fbaCustom = fbaRoot + 'fba2x.cfg'
 fbaCustomOrigin = fbaRoot + 'fba2x.cfg.origin'
 fba2xBin = '/usr/bin/fba2x'
 
-mupenCustom = "/recalbox/share/configs/mupen64/mupen64plus.cfg"
+mupenCustom = "/recalbox/share/system/configs/mupen64/mupen64plus.cfg"
 
 shaderPresetRoot = "/recalbox/share/configs/shadersets/"
 
 kodiJoystick = '/recalbox/share/system/.kodi/userdata/keymaps/recalbox.xml'
-kodiMapping  = '/recalbox/share/configs/kodi/input.xml'
+kodiMapping  = '/recalbox/share/system/configs/kodi/input.xml'
 
 kodiBin  = '/usr/lib/kodi/kodi.bin'
