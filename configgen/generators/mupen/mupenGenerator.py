608c55bb681667a6e1fe65e328676a3a99deb391
Subs <substring@users.noreply.github.com>
2016-01-05 23:40:47 +0100

    Update mupenGenerator.py
    No more hard coded paths, use mupen specific variables

--- configgen/generators/mupen/mupenGenerator.py
+++ configgen/generators/mupen/mupenGenerator.py
@@ -17,5 +17,5 @@ class MupenGenerator(Generator):
             mupenControllers.writeControllersConfig(playersControllers)
 
         commandArray = ["mupen64plus", "--corelib", "/usr/lib/libmupen64plus.so.2.0.0", "--gfx", "/usr/lib/mupen64plus/mupen64plus-video-{}.so".format(system.config['core']),
-                        "--configdir", "/recalbox/share/system/configs/mupen64/", "--datadir", "/recalbox/share/system/configs/mupen64/", rom]
+                        "--configdir", recalboxFiles.mupenConf, "--datadir", recalboxFiles.mupenConf, rom]
         return Command.Command(videomode=system.config['videomode'], array=commandArray, env={"SDL_VIDEO_GL_DRIVER":"/usr/lib/libGLESv2.so"})
