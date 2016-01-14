ec006656f70ab8d6bbafbda44498484c3288d8a2
Subs <frog2wah@gmail.com>
2016-01-14 22:20:06 +0100

    Création du configgen de ScummVM, basé sur sa version d'origine en shell

---
+++ configgen/generators/scummvm/scummvmGenerator.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+import Command
+import recalboxFiles
+from generators.Generator import Generator
+import os.path
+
+
+class ScummVMGenerator(Generator):
+    # Main entry of the module
+    # Configure mupen and return a command
+    def generate(self, system, rom, playersControllers):
+        # Settings recalbox default config file if no user defined one
+        if not system.config['configfile']:
+            # Using recalbox config file
+            #system.config['configfile'] = recalboxFiles.mupenCustom
+            pass
+        
+        # Find rom path
+        romPath = os.path.dirname(rom)
+        romName = os.path.splitext(os.path.basename(rom))[0]
+        # Get rom name without extension
+        print romPath
+        print romName
+        commandArray = ["scummvm", 
+			"--joystick=0", 
+			"--screenshotspath=/recalbox/share/screenshots", 
+			"--extrapath=/usr/share/scummvm",
+			"--path=""{}""".format(romPath),
+			"""{}""".format(romName)]
+			
+        return Command.Command(videomode='default', array=commandArray, env={"SDL_VIDEO_GL_DRIVER":"/usr/lib/libGLESv2.so"})
