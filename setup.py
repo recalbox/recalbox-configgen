401c665cca7805576876ab77245959e0cd7705fd
Laurent Merckx <laurent-merckx@skynet.be>
2016-03-04 15:44:16 +0100

    Add dosbox support in emulatorLauncher

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ from distutils.core import setup
 setup(name='recalbox-configgen',
       version='1.0',
       py_modules=['configgen'],
-      packages=['configgen', 'configgen.generators', 'configgen.generators.libretro', 'configgen.generators.fba2x', 'configgen.generators.mupen', 'configgen.generators.kodi', 'configgen.generators.moonlight', 'configgen.generators.scummvm','configgen.settings','configgen.utils'],
+      packages=['configgen', 'configgen.generators', 'configgen.generators.libretro', 'configgen.generators.fba2x', 'configgen.generators.mupen', 'configgen.generators.kodi', 'configgen.generators.moonlight', 'configgen.generators.scummvm','configgen.generators.dosbox','configgen.settings','configgen.utils'],
       )
