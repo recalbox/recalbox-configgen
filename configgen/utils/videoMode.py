a761c0e846d5c341d8e5b8ecc5150cdf394f0bc5
Subs <frog2wah@gmail.com>
2016-12-30 22:19:11 +0100

    videoMode : allow new hdmi_cvt and hdmi_timings modes

--- configgen/utils/videoMode.py
+++ configgen/utils/videoMode.py
@@ -2,6 +2,7 @@
 import os
 import sys
 import recalboxFiles
+import re
 from settings.unixSettings import UnixSettings
 
 # Set a specific video mode
@@ -9,7 +10,13 @@ def setVideoMode(videomode):
     os.system(createVideoModeLine(videomode))
 
 def createVideoModeLine(videoMode):
-    return "tvservice -e '{}'".format(videoMode)
+    # pattern (CEA|DMT) [0-9]{1,2} HDMI
+    if re.match("^(CEA|DMT) [0-9]{1,2}( HDMI)?$", videoMode):
+        return "tvservice -e '{}'".format(videoMode)
+    if re.match("^hdmi_cvt [\d\s]{10,20}$", videoMode):
+        return "vcgencmd {} && tvservice -e 'DMT 87'".format(videoMode)
+    if re.match("^hdmi_timings [\d\s]{48,58}$", videoMode):
+        return "vcgencmd {} && tvservice -e 'DMT 87'".format(videoMode)
 
 # Set a specific video mode
 def isSupported(index, mode="CEA", drive="HDMI"):
