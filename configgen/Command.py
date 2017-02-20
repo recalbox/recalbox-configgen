70bf28bbdab67d17e3c36a390fd14c0ed3d1c9b7
digitalLumberjack <digitallumberjack@gmail.com>
2017-02-20 07:47:21 +0100

    added default delay to fix blackscreen

--- configgen/Command.py
+++ configgen/Command.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 class Command:
-    def __init__(self, videomode, array, env=dict(), delay = None):
+    def __init__(self, videomode, array, env=dict(), delay = 0.5):
         self.videomode = videomode
         self.array = array
         self.env = env
